1fd2bbb764ada89bf85766ede6a1a8f449d70d25
suku <sukumarreddyp06@gmail.com>
2026-07-22 23:26:31 +0530

    Add PWA support and fix service worker installation

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,41 +1,9 @@
-const CACHE_NAME = "freight-crm-v1";
+const CACHE_NAME = "freight-crm-v2";
 
-const urlsToCache = [
-    "/",
-    "/static/css/crm.css",
-    "/static/js/crm.js",
-    "/static/manifest.json"
-];
-
-// Install
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
 });
 
-// Fetch
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-// Activate
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
 });
